9edf5e0b5bf5b2a8aa1ca107dd0ca117bef103a7
nam2184 <namkhanh.cao123@gmail.com>
2025-09-24 10:22:23 +1000

    debug

--- inference-server.py
+++ inference-server.py
@@ -72,7 +72,7 @@ def generate_server_configs(network : Network):
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
     else:
-        print("Download failed:", response.status_code)
+        print("Download failed:", response.status_code, response.json())
     time.sleep(10)
 
 
